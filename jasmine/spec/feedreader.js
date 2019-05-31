653490d1fb485ae0cc85b68c0308d3444b853b4b
William Lockhart <brownshoe@Williams-MBP.attlocal.net>
2019-05-30 20:09:16 -0500

    Finished v1 of feedreader.js + new readme.md

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -19,7 +19,7 @@ $(function() {
          * empty. Experiment with this before you get started on
          * the rest of this project. What happens when you change
          * allFeeds in app.js to be an empty array and refresh the
-         * page?
+         * page? [It fails the "not.toBe(0)"" test" - WML]
          */
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
@@ -27,46 +27,126 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* TODO-4: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('has feeds each with URLs that are defined and not empty', function() {
+            allFeeds.forEach(function(feedItem) {
+                var urlTest = feedItem.url;
+                expect(urlTest).toBeDefined;
+                expect(urlTest).not.toBe('');
+            });
+        });
 
-
-        /* TODO: Write a test that loops through each feed
+        /* TODO-5: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('has feeds each with names that are defined and not empty', function() {
+            allFeeds.forEach(function(feedItem) {
+                var urlTest = feedItem.url;
+                expect(urlTest).toBeDefined;
+                expect(urlTest).not.toBe('');
+            });
+        });
     });
+        
 
-
-    /* TODO: Write a new test suite named "The menu" */
-
-        /* TODO: Write a test that ensures the menu element is
+    /* TODO-6: Write a new test suite named "The menu" */
+    describe('The menu', function() {
+        /* TODO-7: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        var slideMenu = document.querySelector('.slide-menu'),
+            menuIcon = document.querySelector('.menu-icon-link'),
+            offscreenXPosition = -192,
+            onscreenXPosition = 0;
 
-         /* TODO: Write a test that ensures the menu changes
+        it('is hidden by default', function() {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+            expect(slideMenu.getBoundingClientRect().x).toBe(offscreenXPosition);
+        });
+
+
+         /* TODO-8: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+        describe('and upon click', function(){
+            
+            beforeEach(function(done) {
+                menuIcon.click();
+                // Need time to let the transforms complete before checking moved state
+                setTimeout(function() {
+                    done();
+                },300);
+            
+            });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+            it('actually moves onscreen', function(done) {
+                expect($('body').hasClass('menu-hidden')).toBe(false);
+                expect(slideMenu.getBoundingClientRect().x).toBe(onscreenXPosition);
+                done();
+            });
 
-        /* TODO: Write a test that ensures when the loadFeed
+            it('actually moves back offscreen', function(done) {
+                expect($('body').hasClass('menu-hidden')).toBe(true)
+                expect(slideMenu.getBoundingClientRect().x).toBe(offscreenXPosition);
+                done();
+            });
+        });
+    });
+    /* TODO-9: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
+        /* TODO-10: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+         var entriesLength = 0;
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+        beforeEach(function(done) {
+            var container = $('.feed');
+            container.empty();
+            loadFeed(0,done);
+        });
 
-        /* TODO: Write a test that ensures when a new feed is loaded
+        it('have at least one entry', function(done) {
+            var entries = document.querySelectorAll('.entry-link');
+            expect(entries.length).not.toBe(0);
+            done();
+        });
+    }); 
+    /* TODO-11: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
+        /* TODO-12: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        var container = $('.feed'),
+            firstContainer = container;
+
+        beforeEach(function(done) {
+            container.empty();
+            loadFeed(0);
+            firstContainer = $('.feed');
+            loadFeed(1,done);
+        });
+
+        afterEach(function(done) {
+            loadFeed(0,done);
+        });
+
+        it('changes content on the page', function(done) {
+            var secondContainer = $('.feed');
+            expect(firstContainer).not.toBe(secondContainer);
+            done();
+        });
+
+    }); 
 }());
